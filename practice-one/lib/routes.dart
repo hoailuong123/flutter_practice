f12e2a97dda4e4d314e89d58d557a6e8c8a9419c
HoaiLuong <hoailuong160202@gmail.com>
2023-09-12 21:08:17 +0700

    update routes

--- practice-one/lib/routes.dart
+++ practice-one/lib/routes.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:practice-one/lib/screens/cart/cart_screen.dart';
-import 'package:practice-one/lib/screens/congrats/congrats_screen.dart';
-import 'package:practice-one/lib/screens/home/home_screen.dart';
-import 'package:practice-one/lib/screens/profile/profile_screen.dart';
-import 'package:practice-one/lib/screens/log_in/log_in_screen.dart';
-import 'package:practice-one/lib/screens/product/product_screen.dart';
-import 'package:practice-one/lib/screens/welcome/welcome_screen.dart';
-import 'package:practice-one/lib/screens/check_out/check_out_screen.dart';
-import 'package:practice-one/lib/screens/setting/setting_screen.dart';
+import './screens/cart/cart_screen.dart';
+import './screens/congrats/congrats_screen.dart';
+import './screens/home/home_screen.dart';
+import './screens/profile/profile_screen.dart';
+import './screens/log_in/log_in_screen.dart';
+import './screens/product/product_screen.dart';
+import './screens/welcome/welcome_screen.dart';
+import './screens/check_out/check_out_screen.dart';
+import './screens/setting/setting_screen.dart';
 
 
 final Map<String, WidgetBuilder> routes = {
