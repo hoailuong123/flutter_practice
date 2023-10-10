d253a55e7e57e122bdff32621a62c99e126ca423
HoaiLuong <hoailuong160202@gmail.com>
2023-10-10 23:43:41 +0700

    implement UI home screen

--- practice-one/lib/screens/home/home_screen.dart
+++ practice-one/lib/screens/home/home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:Flutter_Application_1/screens/home/components/categories.dart';
 import 'package:flutter/material.dart';
 import 'components/top_bar.dart';
+import 'package:Flutter_Application_1/main.dart';
 
 class HomeScreen extends StatelessWidget {
   static String routeName = "/home";
@@ -10,119 +12,59 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            TopBar(),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: CategoryList(),
-            ),
-            Container(
-              width: 157,
-              height: 236,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(
-                      "assets/images/jonny-caspari-wsvCC6UyKjs-unsplash 1.png"),
-                  fit: BoxFit.fill,
-                ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TopBar(),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: CategoryList(),
               ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+              GridView.count(
+                crossAxisCount: 2,
+                childAspectRatio: 0.75,
+                shrinkWrap: true,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 5.0,
+                children: [
+                  for (int i = 1; i <= 4; i++)
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 20.0),
+                      child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.white.withOpacity(0.5),
+                                  spreadRadius: 1,
+                                )
+                              ]),
+                          child: Image.asset(
+                            "assets/images/$i.png",
+                            width: 157,
+                            height: 200,
+                          )
 
-class CategoryList extends StatefulWidget {
-  const CategoryList({
-    super.key,
-  });
-
-  @override
-  State<CategoryList> createState() => _CategoryListState();
-}
-
-class _CategoryListState extends State<CategoryList> {
-  int selectedIndex = 0;
-  final List<Widget> categoryIcons = <Widget>[
-    Image.asset("assets/icons/star 1.png"),
-    Image.asset("assets/icons/chair 4.png"),
-    Image.asset("assets/icons/table 1 1.png"),
-    Image.asset("assets/icons/sofa 1.png"),
-    Image.asset("assets/icons/bed 1.png"),
-    Image.asset("assets/icons/lamp 1.png"),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 44,
-      child: ListView.separated(
-        shrinkWrap: true,
-        itemBuilder: (context, index) {
-          return CategoryIcon(
-            onTab: () {
-              if (selectedIndex != index) {
-                setState(() {
-                  selectedIndex = index;
-                });
-              }
-            },
-            icon: categoryIcons[index],
-            color:
-                selectedIndex == index ? Color(0xFF4F4F4F) : Color(0xFFBDBDBE),
-            shadow: selectedIndex == index
-                ? [
-                    BoxShadow(
-                      color: Color(0x3F000000),
-                      blurRadius: 4,
-                      offset: Offset(0, 4),
-                      spreadRadius: 0,
+                          // child: Column(children: [
+                          //   InkWell(
+                          //     onTap: () {},
+                          //     child: Container(
+                          //       padding: EdgeInsets.all(10),
+                          //       child: Image.asset("assets/images/$i.png"),
+                          //       width: 157,
+                          //       height: 200,
+                          //     ),
+                          //   ),
+                          // ]),
+                          ),
                     ),
-                  ]
-                : [],
-          );
-        },
-        scrollDirection: Axis.horizontal,
-        separatorBuilder: (context, index) {
-          return SizedBox(width: 25);
-        },
-        itemCount: categoryIcons.length,
-      ),
-    );
-  }
-}
-
-class CategoryIcon extends StatelessWidget {
-  const CategoryIcon({
-    super.key,
-    required this.onTab,
-    required this.icon,
-    required this.color,
-    required this.shadow,
-  });
-  final Function()? onTab;
-  final Widget? icon;
-  final Color? color;
-  final List<BoxShadow> shadow;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 44,
-      height: 44,
-      child: InkWell(
-        child: icon,
-        onTap: onTab,
-      ),
-      decoration: ShapeDecoration(
-        color: color,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
+                ],
+              ),
+            ],
+          ),
         ),
-        shadows: shadow,
       ),
     );
   }
